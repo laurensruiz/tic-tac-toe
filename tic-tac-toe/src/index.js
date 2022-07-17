3687f2af81776f10d90ff294cde52600f8af333d
laurensruiz <laurenruiz97@yahoo.com>
2022-07-17 16:04:49 -0400

    set winner message

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -18,17 +18,33 @@ function Square(props){
         this.state ={
             squares: Array(9).fill(null),
             xIsNext: true,
+            message: null,
         }
     }
 
     //handler
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i]= this.state.xIsNext? 'X': 'O';
+        if (squares[i] !== null){
+            return squares[i]
+        } else{
+            squares[i]= this.state.xIsNext? 'X': 'O';
+        }
+        
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
         })
+
+        if((squares[0]==='X' && squares[1]==='X' && squares[2]==='X') || (squares[0]==='X' && squares[3]==='X' && squares[6]==='X') || (squares[3]==='X' && squares[4]==='X' && squares[5]==='X')|| (squares[6]==='X' && squares[7]==='X' && squares[8]==='X')|| (squares[1]==='X' && squares[4]==='X' && squares[7]==='X')|| (squares[2]==='X' && squares[5]==='X' && squares[8]==='X')|| (squares[0]==='X' && squares[4]==='X' && squares[8]==='X')|| (squares[2]==='X' && squares[4]==='X' && squares[6]==='X')){
+            this.setState({
+                message: 'X wins!'
+            })
+        } else if ((squares[0]==='O' && squares[1]==='O' && squares[2]==='O') || (squares[0]==='O' && squares[3]==='O' && squares[6]==='O') || (squares[3]==='O' && squares[4]==='O' && squares[5]==='O')|| (squares[6]==='O' && squares[7]==='O' && squares[8]==='O')|| (squares[1]==='O' && squares[4]==='O' && squares[7]==='O')|| (squares[2]==='O' && squares[5]==='O' && squares[8]==='O')|| (squares[0]==='O' && squares[4]==='O' && squares[8]==='O')|| (squares[2]==='O' && squares[4]==='O' && squares[6]==='O')){
+            this.setState({
+                message: 'O wins!'
+            })
+        }
     }
     renderSquare(i) {
       return <Square 
@@ -38,11 +54,12 @@ function Square(props){
   
     render() {
       const status = 'Next player:' + (this.state.xIsNext? 'X' : 'O');
-      
+
   
       return (
         <div>
           <div className="status">{status}</div>
+          <div className="message">{this.state.message}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
